4880fc884eac7d05b36d46458ecfe647b1823d74
王立斌 <22376204@buaa.edu.cn>
2025-12-16 23:49:29 +0800

    hello

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.yunhang.forum {
 
     // Data / utils
     requires com.google.gson;
+    requires jakarta.mail;
 
     // Optional standard modules (uncomment if used)
     // requires java.sql;
@@ -24,4 +25,5 @@ module com.yunhang.forum {
     opens com.yunhang.forum.model.entity to javafx.fxml;
     opens com.yunhang.forum.model.enums to javafx.fxml;
     opens com.yunhang.forum.controller.post to javafx.fxml;
+    opens com.yunhang.forum.controller.main to javafx.fxml;
 }
